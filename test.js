4066531609022a4a8f298d598683708aa2130a5f
Matouš Marek <mylapqn312@gmail.com>
2020-10-20 03:39:31 +0200

    f

--- test.js
+++ test.js
@@ -346,7 +346,7 @@ client.on('message', message => {
           let mentionUsers = "";
           let m = {};
           message.channel.members.each(u => {
-            console.log(u);
+            console.log(u.username);
             m[u.username] = 0;
           });
           console.log(m);
