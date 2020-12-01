ce43d0792e8e4f3cdc7b97e5809437b361259c88
Matouš Marek <mylapqn312@gmail.com>
2020-12-02 00:37:49 +0100

    f

--- test.js
+++ test.js
@@ -588,7 +588,7 @@ client.on('message', message => {
         case "kinoPlaylist": {
           message.delete();
           if (kinoPlaylist.size > 0) {
-            let newMessage = "**Film suggestions:**\n✅ - Watched, <:white_cross:767907092907687956> - Not watched\n----------------";
+            let newMessage = "**Film suggestions:**\n✅ - Watched, <:white_cross:767907092907687956> - Not watched\n\n";
             kinoPlaylist.forEach(f => {
               if (f.watched) newMessage += "✅ "
               else newMessage += "<:white_cross:767907092907687956> ";
