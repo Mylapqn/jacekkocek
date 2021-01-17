22ede6958c0a65c5dbd35f825cd9501fe3f9ec59
Matouš Marek <mylapqn312@gmail.com>
2021-01-17 17:29:26 +0100

    f

--- test.js
+++ test.js
@@ -1339,9 +1339,14 @@ async function load_products(url) {
 	return products;
 }
 
+function filter_stock(products) {
+	return products.filter((product) => product.in_stock);
+}
+
 
 function updateStockInfo() {
-  load_all_products(["https://www.alza.cz/18881565.htm", "https://www.czc.cz/graficke-karty/produkty?q-c-3-f_2027483=sGeForce%20RTX%203090&q-c-0-f_2027483=sGeForce%20RTX%203060%20Ti&q-c-1-f_2027483=sGeForce%20RTX%203070&q-c-2-f_2027483=sGeForce%20RTX%203080"]).then((products) => {
+  load_all_products(["https://www.alza.cz/18881565.htm", "https://www.czc.cz/graficke-karty/produkty?q-c-3-f_2027483=sGeForce%20RTX%203090&q-c-0-f_2027483=sGeForce%20RTX%203060%20Ti&q-c-1-f_2027483=sGeForce%20RTX%203070&q-c-2-f_2027483=sGeForce%20RTX%203080"]).then((productsRaw) => {
+    let products = filter_stock(productsRaw); 
       console.log(products);
       if (stockMessage) {
         let timeString = "\nLAST UPDATE: " + new Date().toLocaleString("cs-CZ", { timeZone: "Europe/Prague" });
