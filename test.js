2002ad120052cf2f5defbb3724badb07939b0751
Matouš Marek <mylapqn312@gmail.com>
2021-01-17 16:39:48 +0100

    f

--- test.js
+++ test.js
@@ -249,35 +249,38 @@ client.on('ready', () => {
       if (stockMessage) {
         let timeString = "\nLAST UPDATE: " + new Date().toLocaleString("cs-CZ", { timeZone: "Europe/Prague" });
         //stockMessage.edit(products + timeString);
+        let embed = new Discord.MessageEmbed({
+          color: 3447003,
+          author: {
+            name: client.user.username,
+            icon_url: client.user.avatarURL()
+          },
+          title: "This is an embed",
+          url: "http://google.com",
+          description: "This is a test embed to showcase what they look like and what they can do.",
+          fields: [{
+            name: "Fields",
+            value: "They can have different fields with small headlines."
+          },
+          {
+            name: "Masked links",
+            value: "You can put [masked links](http://google.com) inside of rich embeds."
+          },
+          {
+            name: "Markdown",
+            value: "You can put all the *usual* **__Markdown__** inside of them."
+          }
+          ],
+          timestamp: new Date(),
+          footer: {
+            icon_url: client.user.avatarURL(),
+            text: "© Example"
+          }
+        });
+        embed.addFields({ name: "Inline Field", value: "They can also be inline.", inline: true });
+
         stockMessage.edit({
-          embed: {
-            color: 3447003,
-            author: {
-              name: client.user.username,
-              icon_url: client.user.avatarURL()
-            },
-            title: "This is an embed",
-            url: "http://google.com",
-            description: "This is a test embed to showcase what they look like and what they can do.",
-            fields: [{
-              name: "Fields",
-              value: "They can have different fields with small headlines."
-            },
-            {
-              name: "Masked links",
-              value: "You can put [masked links](http://google.com) inside of rich embeds."
-            },
-            {
-              name: "Markdown",
-              value: "You can put all the *usual* **__Markdown__** inside of them."
-            }
-            ],
-            timestamp: new Date(),
-            footer: {
-              icon_url: client.user.avatarURL(),
-              text: "© Example"
-            }
-          }.addFields({ name: "Inline Field", value: "They can also be inline.", inline: true })
+          embed: embed
         });
       }
     });
