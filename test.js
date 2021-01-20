65c9596e9c6ec8f86ae05dcd7f1758b0c7c637b8
Matouš Marek <mylapqn312@gmail.com>
2021-01-20 22:07:55 +0100

    f

--- test.js
+++ test.js
@@ -710,8 +710,8 @@ client.on('message', message => {
               message.delete();
               //voice.play("https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_1MG.mp3", { volume: 0.2 });
               //voice.play("http://us4.internet-radio.com:8197/stream", { volume: 0.3 });
-              voice.play("http://ice.actve.net/fm-evropa2-128", { volume: 0.063 });
-              
+              voice.play("http://ice.actve.net/fm-evropa2-128", { volume: 0.3 });
+
             }, function (e) { console.log("REJECTED!!!", e) });
           break;
         }
