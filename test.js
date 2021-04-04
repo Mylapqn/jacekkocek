60ee4285498d04e09127fb560ad3eb4a69e565e6
Matouš Marek <mylapqn312@gmail.com>
2021-04-04 16:58:02 +0200

    f

--- test.js
+++ test.js
@@ -752,7 +752,7 @@ client.on('message', message => {
           if (message.member.voice.channel)
             message.member.voice.channel.join().then(voice => {
               message.delete();
-              voice.play("tududum.mp3", { volume: 0.2 });
+              voice.play("tududum.mp3", { volume: 1 });
 
             }, function (e) { console.log("REJECTED!!!", e) });
           break;
