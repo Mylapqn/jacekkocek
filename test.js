2f75066442deb37e7e98664c1d5b3c01c985bb13
Matouš Marek <mylapqn312@gmail.com>
2021-04-04 13:43:46 +0200

    f

--- test.js
+++ test.js
@@ -775,7 +775,7 @@ client.on('message', message => {
               //voice.play("https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_1MG.mp3", { volume: 0.2 });
               //voice.play("http://us4.internet-radio.com:8197/stream", { volume: 0.3 });
               let num = parseInt(argument);
-
+              console.log(num);
               if (num != NaN && num != "NaN") {
                 if (num < radioStations.length)
                   playStation(voice, num, message.channel);
