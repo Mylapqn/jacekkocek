5e4e46fffe6bc774e84db1b87646990823c601b6
Matouš Marek <mylapqn312@gmail.com>
2021-06-03 17:31:27 +0200

    f

--- test.js
+++ test.js
@@ -1233,8 +1233,8 @@ function playYoutube(argument, channel) {
         videoStream.on("finish", () => {
         });
         clearYoutubeTimeout();
-        nextYoutube = setTimeout(() => { playYoutube(nextUrl, channel) }, (2+length) * 1000);
-        channel.send(((2+length) * 1000));
+        nextYoutube = setTimeout(() => { playYoutube(nextUrl, channel) }, (parseInt(length)+2) * 1000);
+        channel.send(((parseInt(length)+2) * 1000));
       }
     })
   }
