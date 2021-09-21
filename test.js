778a1e1d1dcb612b5bb312127196216adcbf83f8
Matouš Marek <mylapqn312@gmail.com>
2021-09-21 21:14:40 +0200

    f

--- test.js
+++ test.js
@@ -339,7 +339,7 @@ function registerCommands() {
       }
     ]
   }
-  axios.request({ headers: { "Authorization": "Bot " + process.env.DISCORD_BOT_TOKEN }, data: data, url: url, method: "post" }).then(function (response) { //console.log(response) });
+axios.request({ headers: { "Authorization": "Bot " + process.env.DISCORD_BOT_TOKEN }, data: data, url: url, method: "post" }).then(function (response) { /*console.log(response) */});
 }
 
 
