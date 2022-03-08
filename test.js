a8169c972e8172e2a3b43ba93a5bd3e6774dd9dd
Matouš Marek <mylapqn312@gmail.com>
2022-03-08 01:52:47 +0100

    f

--- test.js
+++ test.js
@@ -264,7 +264,7 @@ var radioTimer;
 var fluttershy = true;
 var radioApiKey;
 var radioServerPing = 0;
-radioApiKey();
+radioApiKeyGet();
 
 var nextYoutube;
 var nextYoutubeData;
@@ -1874,7 +1874,7 @@ function mlpSong(voice, index, autoplay, channel) {
 }
 
 function playRadio(voice, channel) {
-  Https.get("https://ponyweb.ml/api.php?stream&key=" + radioApiKey(), function (res) {
+  Https.get("https://ponyweb.ml/api.php?stream&key=" + radioApiKey, function (res) {
     console.log("HTTPS status:" + res.statusCode);
     var body;
     res.on("data", function (data) {
@@ -1913,7 +1913,7 @@ function playRadio(voice, channel) {
   });
 }
 
-function radioApiKey() {
+function radioApiKeyGet() {
   Https.get("https://ponyweb.ml/api.php?keyrequest", function (res) {
     let startTime = Date.now();
     console.log("HTTPS status:" + res.statusCode);
