ec64108fa1939395152e349a71f579107abd54d6
Matouš Marek <mylapqn312@gmail.com>
2022-03-29 19:58:57 +0200

    f

--- database.js
+++ database.js
@@ -1,5 +1,6 @@
 import mysql from "promise-mysql";
 import { stockPresets } from "./stockPresets.js";
+import * as Utilities from "./utilities.js";
 
 /**@type {mysql.Connection} */
 let connection;
@@ -8,9 +9,9 @@ export async function init() {
 }
 
 async function createUser(id) {
-    await connection.query(`INSERT INTO Users VALUES (\"${id}\",0)`);
+    await connection.query(`INSERT INTO Users VALUES (\"${id}\",0)`).catch(e=>{console.log("User creation error: ",e)});
     for (const stockPreset of stockPresets) {
-        await connection.query(`INSERT INTO Wallet (user, currency, amount) VALUES (\"${id}\",\"${stockPreset.id}\",0)`);
+        await connection.query(`INSERT INTO Wallet (user, currency, amount) VALUES (\"${id}\",\"${stockPreset.id}\",0)`).catch(e=>{console.log("Wallet creation error: ",e)});
     }
     return await connection.query(`SELECT * FROM Users WHERE id=\"${id}\"`);
 }
@@ -23,20 +24,22 @@ export async function getUser(id) {
     userData = userData[0];
     let walletsData = await connection.query(`SELECT * FROM Wallet WHERE user=\"${id}\"`);
     let wallets = new Map();
-    stockPresets.forEach(preset=>{
-        wallets.set(preset.id,0);
+    stockPresets.forEach(preset => {
+        wallets.set(preset.id, 0);
     })
     walletsData.forEach(wallet => {
-        wallets.set(wallet.currency,wallet.amount);
+        wallets.set(wallet.currency, wallet.amount);
     });
     userData.wallets = wallets;
     return userData;
 }
 
-export async function setUser(user){
-    connection.query(`UPDATE Users SET matoshi=${user.matoshi} WHERE id=\"${user.id}\"`);
-    for (const [currency,amount] of user.wallets) {
-        console.log(currency,amount)
-        connection.query(`INSERT INTO Wallet (user, currency, amount) VALUES (\"${user.id}\",\"${currency}\",${amount}) ON DUPLICATE KEY UPDATE amount = ${amount}`);
+export async function setUser(user) {
+    if (Utilities.isValid(user.matoshi))
+        connection.query(`UPDATE Users SET matoshi=${user.matoshi} WHERE id=\"${user.id}\"`).catch(e=>{console.log("Matoshi update error: ",e)});
+    for (const [currency, amount] of user.wallets) {
+        console.log(currency, amount)
+        if (Utilities.isValid(amount))
+            connection.query(`INSERT INTO Wallet (user, currency, amount) VALUES (\"${user.id}\",\"${currency}\",${amount}) ON DUPLICATE KEY UPDATE amount = ${amount}`).catch(e=>{console.log("Wallet update error: ",e)});
     }
 }
