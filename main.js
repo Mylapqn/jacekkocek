ed09e8c7d4bb228e2d90b8eb084c08fe7aa20f63
Matouš Marek <mylapqn312@gmail.com>
2022-05-19 17:12:11 +0200

    f

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ intents.add(Intents.FLAGS.GUILD_VOICE_STATES);
 intents.add(Intents.FLAGS.GUILD_MEMBERS);
 export const client = new Discord.Client({ intents: intents });
 
-const updateGlobalCommands = false;
+const updateGlobalCommands = true;
 const commandsToDeleteGlobal = [];
 const commandsToDeleteGuild = [];
 /**
