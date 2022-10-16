ffbc9f4cfe82d511b2dddf05df0dfd27551c3254
Matouš Marek <mylapqn312@gmail.com>
2022-10-16 19:25:36 +0200

    f
    Co-authored-by: NotRustyBot <NotRustyBot@users.noreply.github.com>

--- src/database.ts
+++ src/database.ts
@@ -107,6 +107,7 @@ export class PollDatabase {
             let poll = new Polls.Poll(pollRow["name"])
             poll.id = pollRow["id"];
             poll.message = await messageFromUid(pollRow["message_id"]);
+            console.log(poll.message);
             if (!poll.message) {
                 PollDatabase.deletePoll(poll);
                 console.log("Deleted poll with missing message");
