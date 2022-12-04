c7bd155bd3fc7a7e7aa3e412fcc0bf63bb0f3e1c
Andrej Karovin <andrej.karovin2000@gmail.com>
2022-12-05 00:26:45 +0100

    Merge

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ import * as Polls from "./polls";
 import * as Kino from "./kino";
 import * as Sheets from "./sheets";
 import { handleMessageReaction } from "./reactions";
+import { getStockChoices, getStockFeeHints } from "./stockPresets";
 
 //const icecastParser = require("icecast-parser");
 //const Parser = icecastParser.Parser;
@@ -61,7 +62,14 @@ httpServer.use(express.json());
 export async function setPolicyValue(name: string, value: number) {
   let [category, policy] = name.split(".");
   policyValues[category][policy] = value;
-  Database.PolicyDatabase.setPolicy(name, value);
+  let promises = [];
+  if (policyRelation[category] != undefined && policyRelation[category][policy] != undefined) {
+    for (const related of policyRelation[category][policy]) {
+      promises.push(setPolicyValue(related, value));
+    }
+  }
+  await Promise.all(promises);
+  await Database.PolicyDatabase.setPolicy(name, value);
 }
 
 export function getPolicyValue(name: string) {
@@ -83,43 +91,66 @@ export function generatePolicyList() {
     list += "**" + Utilities.toTitleCase(category) + ":**\n";
     for (const policy in policyValues[category]) {
       const value = policyValues[category][policy];
-      list += "• " + policyNames[category][policy][name] + ": **" + value + " " + policyNames[category][policy][unit] + "**\n";
+      if (policyInfluence[category + "." + policy] == null || getPolicyValue(policyInfluence[category + "." + policy]) != value)
+        list += "• " + policyNames[category][policy][name] + ": **" + value + " " + policyNames[category][policy][unit] + "**\n";
     }
   }
   return list;
 }
 
+export let policyRelation = {};
+
+let policyInfluence = {}
+
+function preparePolicyInfluence() {
+  policyInfluence = (() => {
+    let result = {};
+    for (const category in policyRelation) {
+      for (const policy in policyRelation[category]) {
+        const relations = policyRelation[category][policy];
+        for (const name of relations) {
+          result[name] = category + "." + policy;
+        }
+      }
+    }
+    return result;
+  })();
+}
+
 export let policyValues = {
-  matoshi: {
-    transactionFeePercent: 0,
-    transactionFeeMin: 1,
-    stockFee: 0.5,
-    weeklyTaxPercent: 0,
-    weeklyTaxFlat: 0,
-  },
   kino: {
     suggestReward: 50,
     watchReward: 200,
     lateFee: 100,
-    defaultTimeHrs: 19
-  }
-}
-export let policyNames = {
-  matoshi: {
-    transactionFeePercent: ["Matoshi transaction fee percentage (Doesn't apply if below minimum fee)", "%"],
-    transactionFeeMin: ["Matoshi minimum transaction fee", "₥"],
-    stockFee: ["Stock transaction fee", "%"],
-    weeklyTaxPercent: ["Weekly percent tax", "%"],
-    weeklyTaxFlat: ["Weekly flat tax", "₥"],
+    defaultTimeHrs: 19,
   },
+  matoshi: {
+    transactionFeePercent: 0,
+    transactionFeeMin: 1,
+    weeklyTaxPercent: 0,
+    weeklyTaxFlat: 0,
+  },
+  stock: {
+    defaultFee: 0.5,
+  },
+};
+export let policyNames = {
   kino: {
     suggestReward: ["Kino suggest reward", "₥"],
     watchReward: ["Kino watch reward", "₥"],
-    lateFee: ["Kino late fee", "₥"],
     defaultTimeHrs: ["Kino default time", "hours"],
-  }
-}
-
+    lateFee: ["Kino late fee", "₥"],
+  },
+  matoshi: {
+    transactionFeePercent: ["Matoshi transaction fee percentage (Doesn't apply if below minimum fee)", "%"],
+    transactionFeeMin: ["Matoshi minimum transaction fee", "₥"],
+    weeklyTaxPercent: ["Weekly percent tax", "%"],
+    weeklyTaxFlat: ["Weekly flat tax", "₥"],
+  },
+  stock: {
+    defaultFee: ["Stock transaction fee", "%"],
+  },
+};
 
 
 
@@ -298,6 +329,7 @@ client.on('ready', async () => {
   startDate = new Date();
 
   Stocks.init();
+  preparePolicyInfluence();
   await Database.init();
   Matoshi.init();
   Api.init();
@@ -621,20 +653,16 @@ client.on('interactionCreate', async interaction => {
         break;
       }
       case "policy": {
-        switch (interaction.options.getSubcommand()) {
-          case "edit": {
-            try {
-              let policy = interaction.options.getString("policy");
-              let newValue = interaction.options.getNumber("value");
-              let curValue = getPolicyValue(policy);
-              let policyName = getPolicyName(policy)
-              await setPolicyValue(policy, newValue);
-              interaction.reply({ content: `<@${member.id}> changed the policy **${policyName[0]}** to **${newValue} ${policyName[1]}** (previously ${curValue} ${policyName[1]})`, ephemeral: false, allowedMentions: { users: [], parse: [] } })
-            } catch (error) {
-              interaction.reply({ content: "Policy setting failed!", ephemeral: true });
-            }
-            break;
-          }
+        try {
+          let policy = interaction.options.getString("policy");
+          let newValue = interaction.options.getNumber("value");
+          let curValue = getPolicyValue(policy);
+          let policyName = getPolicyName(policy)
+          await setPolicyValue(policy, newValue);
+          interaction.reply({ content: `<@${member.id}> changed the policy **${policyName[0]}** to **${newValue} ${policyName[1]}** (previously ${curValue} ${policyName[1]})`, ephemeral: false, allowedMentions: { users: [], parse: [] } })
+        } catch (error) {
+          console.log(error);
+          interaction.reply({ content: "Policy setting failed!", ephemeral: true });
         }
         break;
       }
@@ -1324,19 +1352,13 @@ function loadReminders() {
 
 function setupCommands() {
   try {
-    let globalCommands = JSON.parse(fs.readFileSync("globalCommands.json", { encoding: 'utf8' }));
-    let guildCommands = JSON.parse(fs.readFileSync("guildCommands.json", { encoding: 'utf8' }));
-    if (updateGlobalCommands) {
-      client.application?.commands.set(globalCommands);
-      commandsToDeleteGlobal.forEach(na => {
-        afrGuild.commands.delete(na);
-      })
-      console.log("Updated global commands.");
-    }
-    afrGuild.commands.set(guildCommands);
-    commandsToDeleteGuild.forEach(na => {
-      afrGuild.commands.delete(na);
-    })
+    const globalCommands = JSON.parse(fs.readFileSync("globalCommands.json", { encoding: 'utf8' }));
+    const guildCommands = JSON.parse(fs.readFileSync("guildCommands.json", { encoding: 'utf8' }));
+
+    generateGuildCommandOptions(guildCommands);
+
+    await compareCommandsAndApply(client.application?.commands, globalCommands, undefined);
+    await compareCommandsAndApply(client.application?.commands, guildCommands, mainGuild.id);
 
     console.log("Updated guild commands.");
   } catch (error) {
@@ -1345,6 +1367,63 @@ function setupCommands() {
   }
 }
 
+function generateGuildCommandOptions(commands: Array<any>) {
+  let stock = commands.find(o => o.name == "stocks");
+  for (const subcommand of stock.options) {
+    if (["buy", "sell", "info", "balance"].includes(subcommand.name)) {
+      for (const option of subcommand.options) {
+        if (option.name == "stock")
+          option.choices = getStockChoices();
+      }
+    }
+  }
+
+
+  let stockPolicy: { choices: Array<any> } = commands.find(o => o.name == "policy").options.find(o => o.name == "stock").options.find(o => o.name == "policy");
+  stockPolicy.choices = stockPolicy.choices.concat(getStockFeeHints());
+}
+
+type CommandChange = { type: 'delete', command: Discord.ApplicationCommandResolvable } | { type: 'add', command: Discord.ApplicationCommandData } | { type: 'edit', oldCommand: Discord.ApplicationCommandResolvable, newCommand: Discord.ApplicationCommandData };
+
+async function compareCommandsAndApply(manager: Discord.ApplicationCommandManager, targetArray: Discord.ApplicationCommandData[], guildId?: Discord.Snowflake) {
+  const source = await manager.fetch({ guildId: guildId });
+  const target: { [key in string]: Discord.ApplicationCommandData } = targetArray.reduce((map, item) => {
+    map[item.name] = item;
+    return map;
+  }, {});
+
+  // build command changes array
+  const changes: CommandChange[] = [];
+  source.each((command) => {
+    if (command.guildId != guildId)
+      return;
+
+    const targetItem = target[command.name];
+    if (targetItem == null)
+      changes.push({ type: 'delete', command: command });
+    else if (!command.equals(targetItem)) {
+      changes.push({ type: 'edit', oldCommand: command, newCommand: targetItem });
+    }
+
+    delete target[command.name];
+  });
+
+  Object.values(target).forEach((newCommand) => {
+    changes.push({ type: 'add', command: newCommand });
+  });
+
+  // apply command changes
+  const promises = changes.map(async (change) => {
+    if (change.type == 'delete') {
+      await manager.delete(change.command, guildId);
+    } else if (change.type == 'add') {
+      await manager.create(change.command, guildId);
+    } else if (change.type == 'edit') {
+      await manager.delete(change.oldCommand, guildId);
+      await manager.create(change.newCommand, guildId);
+    }
+  });
+}
 
 
 //#endregion
