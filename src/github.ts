0ab146d0e24d490f5ff7020cc8de5a87ca289ecb
Matouš Marek <mylapqn312@gmail.com>
2023-03-02 18:30:40 +0100

    f

--- src/github.ts
+++ src/github.ts
@@ -1,26 +1,43 @@
 import { App, Octokit } from "octokit";
-//import { createAppAuth } from "@octokit/auth-app";
+import { createAppAuth } from "@octokit/auth-app";
+import { request } from "@octokit/request";
 const installationId = 34798513;
 const appId = "300408"
 
-const app = new App({
+/* const app = new App({
     appId: appId,
     privateKey: process.env.GITHUB_PRIVATE_KEY
-});
+}); */
 let octokit: Octokit;
+let token;
 
-export async function init() {
-    octokit = await app.getInstallationOctokit(installationId)
-}
-/*
 const auth = createAppAuth({
     appId: appId,
     privateKey: process.env.GITHUB_PRIVATE_KEY,
     installationId: installationId,
 })
-*/
+
+export async function init() {
+    //octokit = await app.getInstallationOctokit(installationId)
+    token = await auth({ type: "installation" })
+}
+
+
+
 export async function test() {
-    console.log(await octokit.request("POST /repos/Mylapqn/jacekkocek/issues", {
+    await request("POST /repos/Mylapqn/jacekkocek/issues", {
+        headers: {
+            authorization: "token " + token
+        },
+        owner: 'Mylapqn',
+        repo: 'jacekkocek',
+        title: 'Test',
+        body: 'Test desc',
+        labels: [
+            'request'
+        ],
+    });
+    /* console.log(await octokit.request("POST /repos/Mylapqn/jacekkocek/issues", {
         owner: 'Mylapqn',
         repo: 'jacekkocek',
         title: 'Test',
@@ -28,5 +45,5 @@ export async function test() {
         labels: [
           'request'
         ],
-    }));
+    })); */
 }
