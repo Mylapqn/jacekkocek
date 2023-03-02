ad863f78dbc6b7e1620f34ac5a2ee525fd9d376e
Matouš Marek <mylapqn312@gmail.com>
2023-03-02 19:40:35 +0100

    commented github app authentification

--- src/github.ts
+++ src/github.ts
@@ -1,34 +1,35 @@
-import { App, Octokit } from "octokit";
-import { createAppAuth } from "@octokit/auth-app";
-import { request } from "@octokit/request";
-const installationId = 34798513;
-const appId = "300408"
+import { Octokit } from "octokit";
 
-/* const app = new App({
-    appId: appId,
-    privateKey: process.env.GITHUB_PRIVATE_KEY
-}); */
+const installationId = 34798513;
+const appId = 300408
+
 const octokit = new Octokit({
     auth: process.env.GITHUB_PERSONAL_API_KEY
 })
-//let token;
 
-/*const auth = createAppAuth({
+//TODO App authentification
+/*
+
+const app = new App({
+    appId: appId,
+    privateKey: process.env.GITHUB_PRIVATE_KEY
+});
+
+let token;
+const auth = createAppAuth({
     appId: appId,
     privateKey: process.env.GITHUB_PRIVATE_KEY,
     installationId: installationId,
     clientSecret: process.env.GITHUB_API_KEY,
     clientId: "Iv1.5a1e31d3239f66d5",
-})*/
+})
 
 export async function init() {
-    //octokit = await app.getInstallationOctokit(installationId)
-    //token = await auth({ type: "installation" })
+    octokit = await app.getInstallationOctokit(installationId)
+    token = await auth({ type: "installation" })
 }
 
-
-
-/*export async function test() {
+export async function test() {
     await request("POST /repos/Mylapqn/jacekkocek/issues", {
         headers: {
             authorization: "token " + token
@@ -42,6 +43,7 @@ export async function init() {
         ],
     });
 }*/
+
 export async function createIssue(title: string, desc: string, label: string, author: string) {
     let response = await octokit.request("POST /repos/Mylapqn/jacekkocek/issues", {
         owner: 'Mylapqn',
