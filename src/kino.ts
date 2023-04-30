8baed7968728bf638aafa231363852f39b29f30e
Matouš Marek <mylapqn312@gmail.com>
2023-05-01 01:39:33 +0200

    f

--- src/kino.ts
+++ src/kino.ts
@@ -73,6 +73,7 @@ export class Event {
         console.log("kinoreward1");
         const guildEvents = await Main.mainGuild.scheduledEvents.fetch();
         console.log("kinoreward2", guildEvents);
+        
         console.log(this.list);
 
         const activeEvent = this.list.find(k => guildEvents.find((e, id) => k.guildEventId == id && e.isActive()));
